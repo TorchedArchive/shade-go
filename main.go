28f39bbdb9cd1e9547cee687273a5cb6d5826730
mTvare6 <mTvare6@users.noreply.github.com>
2021-05-29 14:04:41 +0530

    feat: query function

--- main.go
+++ main.go
@@ -14,19 +14,23 @@ func isRoot() bool {
 }
 
 func success(s string)  {
-  color.Style{color.Green, color.Bold}.Printf(s)
+  color.Style{color.Green, color.Bold}.Println(s)
 }
 func error(s string)  {
-  color.Style{color.Red, color.Bold}.Printf(s)
+  color.Style{color.Red, color.Bold}.Println(s)
 }
 func main() {
+
   if !(runtime.GOOS == "darwin" || runtime.GOOS == "linux"){
     color.Danger.Println("Shade supports only linux and macOSX")
     os.Exit(0)
   }
+
   if !(isRoot()){
     error("Shade must be run as a root user")
+    os.Exit(0)
   }
+
   packages := os.Args[2:]
   subcommand := os.Args[1]
 
@@ -44,7 +48,7 @@ func main() {
   case "update":
     update()
   case "query":
-    query(os.Args[3])
+    query(os.Args[2])
   default: color.Info.Println("No subcommand called ", subcommand)
   }
 }
--- subcommand.go
+++ subcommand.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+  "os"
+  "log"
+  "fmt"
+  "bufio"
+  "strings"
+)
+
+func rev(s string) (string){
+  return "\033[7m" + s + "\033[0m";
+}
+
 func help(){
 }
 
@@ -19,4 +31,27 @@ func update(){
 }
 
 func query(s string){
+  buildscripts := "/usr/local/shade/main/packages"
+  files, err := os.ReadDir(buildscripts)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  for _, file := range files {
+    path := fmt.Sprintf("%s/%s/meta", buildscripts, file.Name())
+    inFile, err := os.Open(path)
+    if err != nil {
+      fmt.Println(err.Error() + `: ` + path)
+      return
+    }
+    defer inFile.Close()
+
+    scanner := bufio.NewScanner(inFile)
+    for scanner.Scan() {
+      line := scanner.Text()
+      if strings.Contains(line, s){
+        fmt.Println(strings.Replace(line, s, fmt.Sprintf("\033[7m%s\033[0m", s), 1))
+      }
+    }
+  }
 }
