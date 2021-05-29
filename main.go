6648262808918f48a8bf1da59f3ff79c2739320c
sammyette <38820196+TorchedSammy@users.noreply.github.com>
2021-05-29 12:16:15 -0400

    refactor: restyle code and make more idiomatic, remove external dependency

--- main.go
+++ main.go
@@ -1,33 +1,33 @@
 package main
 
-import "runtime"
-import "os"
-import "github.com/gookit/color"
-import "os/user"
+import (
+  "fmt"
+  "os"
+  "runtime"
+  "strings"
+)
 
 func isRoot() bool {
-  currentUser, err := user.Current()
-  if err != nil {
-    color.Error.Println("Unable to get current user: %s", err)
-  }
-  return currentUser.Username == "root"
+  return os.Getuid() == 0
 }
 
-func success(s string)  {
-  color.Style{color.Green, color.Bold}.Println(s)
+func success(s ...string)  {
+  fmt.Printf("\033[32m%s\033[0m", strings.Join(s, " "))
 }
-func error(s string)  {
-  color.Style{color.Red, color.Bold}.Println(s)
+
+func err(s ...string)  {
+  fmt.Printf("\033[31m%s\033[0m", strings.Join(s, " "))
 }
+
 func main() {
-
-  if !(runtime.GOOS == "darwin" || runtime.GOOS == "linux"){
-    color.Danger.Println("Shade supports only linux and macOSX")
-    os.Exit(0)
+  if !(runtime.GOOS == "darwin" || runtime.GOOS == "linux") {
+    err("Shade supports only linux and macOSX")
+    os.Exit(1)
+  }
+  if len(os.Args) < 2 {
+    help()
   }
 
-
-  packages := os.Args[2:]
   subcommand := os.Args[1]
 
   switch subcommand {
@@ -36,15 +36,21 @@ func main() {
   case "setup":
     setup()
   case "install":
+    packages := os.Args[2:]
     install(packages)
   case "uninstall":
+    packages := os.Args[2:]
     uninstall(packages)
   case "upgrade":
+    packages := os.Args[2:]
     upgrade(packages)
   case "update":
     update()
   case "query":
     query(os.Args[2])
-  default: color.Info.Println("No subcommand called ", subcommand)
+  default:
+    err("Unknown command:", subcommand)
+    os.Exit(1)
   }
 }
+
